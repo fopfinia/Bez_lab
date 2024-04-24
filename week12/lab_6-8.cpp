6eef810f14d7d5cb177991e8dc6f1598b1c804ae
Ann <korobeinikova.av@phystech.edu>
2024-04-24 23:01:20 +0300

    add laba labnaya

--- week12/lab_6-8.cpp
+++ week12/lab_6-8.cpp
@@ -5,16 +5,22 @@
 using namespace std;
 #define int long long
 
-const vector<int> ns = {10,   100,  1000, 2000, 3000, 4000,
-                        5000, 6000, 7000, 8000, 9000, 10000};
-const vector<int> k = {10000, 1000, 100, 100, 100, 100,
-                        100, 100, 100, 50, 50, 50};
+const vector<int> ns = {10,   100,  1000, 2000, 4000, 6000, 8000, 10000, 15000,
+                        20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 
+                        65000, 70000, 75000, 80000, 85000, 90000, 95000, 100000};
+const vector<int> k = {100000, 10000, 10000, 10000, 10000, 10000,
+                        10000, 10000, 5000, 5000, 5000, 1000, 1000, 1000,
+                        1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
+                        1000, 1000, 1000, 1000, 1000, 1000, 1000};
 
-vector<int> queue_new(vector<int> &old_mass) {
-    vector<int> new_mass(size(old_mass)*2);
-    for (int i = 0; i < size(old_mass)*2 - 1; i++) new_mass[i] = 0;
+vector<int> queue_new(vector<int>& old_mass, int& write_idx, int& read_idx) {
+    vector<int> new_mass(size(old_mass) * 2);
+    for (int i = 0; i < size(old_mass) * 2 - 1; i++) new_mass[i] = 0;
+    for (int i = 0; i < write_idx; i++) new_mass[i] = old_mass[i];
+    for (int i = read_idx; i < size(old_mass); i++) new_mass[i + size(old_mass)] = old_mass[i];
+    read_idx += size(old_mass);
     return new_mass;
-} 
+}
 
 bool queue_write(vector<int> &mass, int &write_idx, int &read_idx){
     int n = size(mass);
@@ -45,25 +51,27 @@ signed main(){
     for (int i = 0; i < ns.size(); ++i) {
         int n = ns[i];
         for (int j = 0; j < k[i]; ++j) {  
-            int write_idx = 0, read_idx = 0;
-            vector<int> mass(1);
+            int write_idx = 1, read_idx = 0;
+            vector<int> mass(2);
             auto begin = std::chrono::steady_clock::now();
-            if(queue_write(mass, write_idx, read_idx) == false){
-                read_idx = write_idx + size(mass);
-                mass = queue_new(mass); 
-            } 
+            while (size(mass) < n){
+                if(queue_write(mass, write_idx, read_idx) == false){
+                    mass = queue_new(mass, write_idx, read_idx); 
+                } 
+            }
             auto end = std::chrono::steady_clock::now();
             auto time_span =
                 std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
             dp[0][i] += time_span.count();
         }
         for (int j = 0; j < k[i]; ++j) {  
-            int write_idx = 0, read_idx = 0;
-            vector<int> mass(1);
+            int write_idx = 1, read_idx = 0;
+            vector<int> mass(2);
             auto begin = std::chrono::steady_clock::now();
-            if(queue_read(mass, write_idx, read_idx) == false){
-                write_idx = read_idx + size(mass);
-                mass = queue_new(mass); 
+            while (size(mass) < n){
+                if(queue_read(mass, write_idx, read_idx) == false){
+                    mass = queue_new(mass, write_idx, read_idx); 
+                }
             }
             auto end = std::chrono::steady_clock::now();
             auto time_span =
--- week12/test.cpp
+++ week12/test.cpp
@@ -5,39 +5,56 @@
 using namespace std;
 #define int long long
 
-const vector<int> ns = {10,   100,  1000, 2000, 3000, 4000,
-                        5000, 6000, 7000, 8000, 9000, 10000};
-const vector<int> k = {10000, 1000, 100, 100, 100, 100,
-                        100, 100, 100, 50, 50, 50};
-
-vector<int> queue_new(vector<int> &old_mass) {
-    vector<int> new_mass(size(old_mass)*2);
-    for (int i = 0; i < size(old_mass)*2 - 1; i++) new_mass[i] = 0;
+vector<int> queue_new(vector<int>& old_mass, int& write_idx, int& read_idx) {
+    vector<int> new_mass(size(old_mass) * 2);
+    for (int i = 0; i < size(old_mass) * 2 - 1; i++) new_mass[i] = 0;
+    for (int i = 0; i < write_idx; i++) new_mass[i] = old_mass[i];
+    for (int i = read_idx; i < size(old_mass); i++) new_mass[i + size(old_mass)] = old_mass[i];
+    read_idx += size(old_mass);
     return new_mass;
-} 
+}
 
-bool queue_write(vector<int> &mass, int &write_idx, int &read_idx){
+bool queue_write(vector<int>& mass, int& write_idx, int& read_idx) {
     int n = size(mass);
-    if (write_idx < n-1){
-        if(write_idx != read_idx){
+    if (write_idx < n - 1) {
+        if (write_idx != read_idx) {
+            mass[write_idx] = 1;
             write_idx++;
             return true;
         }
     }
-    else{
+    else {
+        mass[write_idx] = 1;
         write_idx = 0;
         return true;
     }
     return false;
 }
+bool queue_read(vector<int>& mass, int& write_idx, int& read_idx) {
+    int n = size(mass);
+    if (read_idx < n - 1) {
+        read_idx++;
+        return true;
+    }
+    return false;
+}
 
-int main(){
-    vector<int> mass(1);
-    int write_idx = 0, read_idx = 0;
-    if(queue_write(mass, write_idx, read_idx) == false){
-        read_idx = write_idx + size(mass);
-        mass = queue_new(mass); 
-    } 
-    cout << size(mass) << ' ' << read_idx;
+signed main() {
+    std::chrono::microseconds arr[100];
+    for (int k = 1; k < 1000; k++)
+    {
+        vector<int> mass(2);
+        int write_idx = 1, read_idx = 0;
+        auto begin = std::chrono::steady_clock::now();
+        for (int i = 0; i < 1000 * k; i++)
+        {
+            if (queue_write(mass, write_idx, read_idx) == false) {
+                mass = queue_new(mass, write_idx, read_idx);
+            }
+        }
+        auto end = std::chrono::steady_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        cout << time_span.count() << ", ";
+    }
     return 0;
 }
